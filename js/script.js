db980193be445c6a7514a72c3a2b200bd4fba4a0
rovinimartina <martinarovini@gmail.com>
2024-09-01 23:17:15 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function AreaCirculo(radio){
+    let area = Math.PI * radio * radio;
+    console.log("El área de un círculo de radio " + radio + " es " + area)
+}
+
+AreaCirculo(20)
+AreaCirculo(6)
+AreaCirculo(117)
